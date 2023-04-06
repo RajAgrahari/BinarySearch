cb576732dbe51090f0edd1018c311b6d0e839378
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2023-04-06 16:33:50 +0530

    Hard

--- MinimumCostToMakeArrayEqual.cpp
+++ MinimumCostToMakeArrayEqual.cpp
@@ -1,30 +1,27 @@
 //https://leetcode.com/problems/minimum-cost-to-make-array-equal/
 long long minCost(vector<int>& nums, vector<int>& cost) {
-        
-        int mx = *max_element(nums.begin(),nums.end());
-        long long mxsum = 0;
-        for(int i=0;i<cost.size();i++)
-        mxsum += (abs(nums[i]-mx)*cost[i]);
-
         long long l = *min_element(nums.begin(),nums.end());
-        long long r=mx;
+        long long r=*max_element(nums.begin(),nums.end());
+        long long sum2=0;
         while(l<=r)
         {
             long long mid = l + (r-l)/2;
             long long sum=0;
+            sum2=0;
             for(int i=0;i<nums.size();i++)
             {
                 sum += (abs(mid-nums[i])*(long long)cost[i]);
+                sum2 += (abs(mid+1-nums[i])*(long long)cost[i]);
             }
-            if(sum<=mxsum)
+            if(sum<=sum2)
             {
                r = mid-1;
-               mxsum = sum;
+               sum2 = sum;
             }
             else{
                 l = mid+1;
             }
         }
-        return r+1;
+        return sum2;
 
     }
